0c3a8f51923fb8e4ee6c83417ed6b4d24bead56f
Akash Munjial <amunjial@umass.edu>
2019-06-20 13:52:55 -0400

    Initial Commit

---
+++ UMASS Access/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UMASS Access
+//
+//  Created by Akash Munjial on 6/20/19.
+//  Copyright © 2019 Akash Munjial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UMASS AccessTests/UMASS_AccessTests.swift
@@ -0,0 +1,34 @@
+//
+//  UMASS_AccessTests.swift
+//  UMASS AccessTests
+//
+//  Created by Akash Munjial on 6/20/19.
+//  Copyright © 2019 Akash Munjial. All rights reserved.
+//
+
+import XCTest
+@testable import UMASS_Access
+
+class UMASS_AccessTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
